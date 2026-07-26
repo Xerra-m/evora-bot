e23dbd7543ed793e20decb73b7f1d969aebea625
Xerra Magani <233551835+Xerra-m@users.noreply.github.com>
2026-07-26 23:21:08 +0700

    feat: add core system and add boom for logger

--- src/commands/ping.js
+++
@@ -1,7 +0,0 @@
-export default {
-  name: "ping",
-
-  async execute() {
-    console.log("Pong! 🏓");
-  },
-};
--- src/evora.js
+++ src/evora.js
@@ -1,28 +1,50 @@
-import ping from "./commands/ping.js";
+// dependencies
+import makeWASocket, {
+  useMultiFileAuthState,
+  DisconnectReason,
+} from "@whiskeysockets/baileys";
+import { Boom } from "@hapi/boom";
+import pino from "pino";
+import qrcode from "qrcode-terminal";
 
 class Evora {
   constructor() {
     this.name = "Evora";
     this.version = "1.0.0";
-    this.commands = new Map();
-  }
-
-  async loadCommands() {
-    this.commands.set(ping.name, ping);
-    console.log("Evora log - Command Loaded!");
+    this.author = "Xerra Magani & Minzy";
+    this.description = "A modular WhatsApp bot build with node.js & baileys";
+    this.sock = null;
   }
 
   async start() {
-    console.log(`Evora log - ${this.name} v${this.version} starting...`);
+    // save cred to session folder in root folder
+    const { state, saveCreds } = await useMultiFileAuthState("../session");
 
-    await this.loadCommands();
+    this.sock = makeWASocket({
+      logger: pino({ level: "silent" }),
+      auth: state,
+    });
 
-    const command = this.commands.get("ping");
+    // handler save creds if there are change
+    this.sock.ev.on("creds.update", saveCreds);
 
-    if (command) {
-      await command.execute();
-    }
+    this.sock.ev.on("connection.update", (update) => {
+      const { connection, lastDisconnect, qr } = update;
+
+      // generate qr di terminal
+      if (qr) {
+        qrcode.generate(qr, { small: true });
+      }
+
+      // auto connect handler
+      if (connection === "close") {
+        const shouldReconnect =
+          (lastDisconnect?.error instanceof Boom)?.output?.statusCode !==
+          DisconnectReason.loggedOut;
+        if (shouldReconnect) this.start();
+      } else if (connection === "open") {
+        console.log(`Evora log - ${this.name} v${this.version} starting...`);
+      }
+    });
   }
 }
-
-export default Evora;
